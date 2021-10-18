3102066e6f3bcebef3e41844b818efce430f59ba
unjalli <75754469+unjalli@users.noreply.github.com>
2021-10-18 13:45:11 -0400

    Find the Root of a tree - Leetcode Problem

---
+++ bst_preorder.py
@@ -0,0 +1,33 @@
+import pdb
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    
+    def _createBST(self, preorder, max_val):
+        pdb.set_trace()
+        print(preorder)
+        print(max_val)
+        if not preorder or preorder[-1]>max_val:
+            return None
+        root = TreeNode(preorder.pop())
+        root.left = self._createBST(preorder, root.val)
+        root.right = self._createBST(preorder, max_val)
+        return root
+
+    def bstFromPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: TreeNode
+        """
+        return self._createBST(preorder[::-1], 10000)
+
+
+preorder = [8, 5, 1, 7, 10, 12]
+root = Solution().bstFromPreorder(preorder)
+print(root)
