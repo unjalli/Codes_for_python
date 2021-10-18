239008a89f427125338bb8fd3516d09ce55d37ec
Aditya Garg <77374278+uhditya@users.noreply.github.com>
2021-10-18 22:38:27 +0530

    Code written
    This code finds the largest prime factor of a given number

--- largest_prime.py
+++ largest_prime.py
@@ -1,1 +1,14 @@
-Please enter code here
+def find_prime(number):
+    for i in range(2,number):
+        if(number%i==0):
+            return 0
+    return 1
+        
+def largest_prime_factor(num):
+    for i in range(num-1,1,-1):
+        if(num%i==0 and find_prime(i)==1):
+            print(i)
+            break
+
+num=int(input())
+largest_prime_factor(num)
